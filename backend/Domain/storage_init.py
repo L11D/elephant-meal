bf35dee2ed084b8c574b5849e6a63c23efb31d1b
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2024-06-24 20:23:53 +0700

    db start

---
+++ backend/Domain/storage_init.py
@@ -0,0 +1,41 @@
+import logging
+
+from models.tables.connected_user import ConnectedUser
+from models.tables.transfering_application import TransferingApplication
+from storage.db_config import SessionLocal, Base, engine
+
+from models.tables.user import User
+from models.tables.classroom import Classroom
+from models.tables.confirm_status import ConfirmStatus
+from models.tables.role import Role
+from models.tables.timeslot import Timeslot
+from models.tables.application import Application
+from models.tables.crl import CRL
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def init_db():
+    db = SessionLocal()
+
+    try:
+        Base.metadata.create_all(bind=engine, tables=[User.__table__,
+                                                      ConnectedUser.__table__,
+                                                      TransferingApplication.__table__,
+                                                      Application.__table__,
+                                                      Classroom.__table__,
+                                                      ConfirmStatus.__table__,
+                                                      Role.__table__,
+                                                      Timeslot.__table__,
+                                                      CRL.__table__,
+                                                      ]
+                                 )
+        db.commit()
+        logger.info("(Init database) Database initialized successfully.")
+    except Exception as e:
+        db.rollback()
+        logger.fatal(f"(Init database) initializing database: {e}")
+        raise
+    finally:
+        db.close()
