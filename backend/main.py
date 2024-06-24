249d869fc407625ac30e877e339cd402421ffa66
Liid <51267703+L11D@users.noreply.github.com>
2024-06-24 20:05:28 +0700

    test bd connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 
+from sqlalchemy import create_engine
+
+username = 'postgres'
+password = '1111'
+host = 'database_host'  # или 'database_host', если запускаете из другого контейнера в одном Compose
+port = '5432'
+database = 'elephant-meal-db'
+
+connection_string = f'postgresql://{username}:{password}@{host}:{port}/{database}'
+
+engine = create_engine(connection_string)
+
+
+
+
 app = FastAPI()
 
 
@@ -12,3 +27,7 @@ async def read_root():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    with engine.connect() as connection:
+        result = connection.execute("SELECT 1")
+        print(result.fetchone())
