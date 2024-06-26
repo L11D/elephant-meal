f4e3b421e1a1144d24cc7197f40b3fdc94810d43
Gegelman Roman Yurievich <gegelmanr@mail.ru>
2024-06-27 06:58:47 +0700

    а

--- backend/Domain/models/tables/category.py
+++ backend/Domain/models/tables/category.py
@@ -11,7 +11,7 @@ class Category(Base):
     __tablename__ = "categories"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    parent_id = Column(UUID(as_uuid=True), ForeignKey('categories.id'), default=uuid.uuid4, nullable=True)
+    parent_id = Column(UUID(as_uuid=True), ForeignKey('categories.id'), nullable=True)
     name = Column(String, nullable=False)
     utility = Column(SQLEnum(Utility), nullable=True)
     is_ready = Column(Boolean, nullable=True)
